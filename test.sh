787b1761ffdbf3be3813f25d397225755706704c
alberto56 <albert@mediatribe.net>
2020-11-01 15:52:49 -0500

    Bugfix: name of temporary image in build

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ docker pull php
 docker build -t local-dcycle-phpunit-image .
 
 docker run --rm -v $(pwd)/example01:/app \
-  local-dcycle-php-image --group myproject
+  local-dcycle-phpunit-image --group myproject
